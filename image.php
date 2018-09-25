f5b233ee56a0ac2a210bf5d8e0a17a85865abfd7
unknown <a.morenoper@gmail.com>
2018-09-25 08:29:43 +0200

    Nuevo código

--- image.php
+++ image.php
@@ -1,4 +1,4 @@
-<?php
+imagefilledrectangle($im, 0, 0, 199, 199, $green);<?php
 // Create a 55x30 image
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
@@ -6,8 +6,8 @@ $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
 
 // Draw a filled rectangle
-imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-//imagefilledrectangle($im, 0, 0, 199, 199, $green);
+//imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+imagefilledrectangle($im, 0, 0, 199, 199, $green);
 
 // Save the image
 header('Content-Type: image/png');
